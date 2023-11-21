19b26d925891bf777fe3c1f902c6e08967109246
agbeje emmanuel <agbejeemmanuel82@gmail.com>
2023-11-21 01:53:12 -1100

    add the function to calculate using the symbol

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+// function to add
+function add(number1, number2) {
+    return number1 + number2;
+  }
+
+  // modulus function
+  function modulus(number1, number2) {
+    return number1 + number2;
+  }
+  //divide function
+ function divide(number1, number2) {
+    return number1 / number2;
+  }
+  //multiple function
+  function multiple(number1, number2) {
+    return number1 * number2;
+  }
+   //substract function
+   function substract(number1, number2) {
+    return number1 - number2;
+  }
+
+  const number1 = parseInt(prompt("Enter a number:"));
+  const number2 = parseInt(prompt("Enter another number:"));
+  
+  //alert(add(number1, number2));
+  //alert(modulus(number1, number2));
+  //alert(divide(number1, number2));
+  //alert(multiple(number1, number2));
+  alert(substract(number1, number2));
